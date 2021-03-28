c6e9afd31074b01ccca9c3827ec2ba02a29b4df2
VCarlosJP <carlosjp.villanueva@gmail.com>
2021-03-29 00:32:35 +0200

    operations component layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MainNav from './components/MainNav/MainNav';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainNav from "./components/MainNav/MainNav";
+import OperationsHeader from "./components/MainContent/operationsHeader";
+import styles from "./styles/app.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <BrowserRouter>
+        <div className="navbar">
           <MainNav />
+        </div>
+        <div className="operations">
+          <OperationsHeader />
+        </div>
       </BrowserRouter>
     </div>
   );
